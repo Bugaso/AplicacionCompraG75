3660dab29a9979ff98885660d6d483b5cc10a636
Bugaso <beelli380@gmail.com>
2023-10-02 20:34:43 -0300

    2/10/2023

---
+++ src/com/Data/DetalleCompraData.java
@@ -0,0 +1,29 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package com.Data;
+
+import com.entidades.Compra;
+import com.entidades.DetalleCompra;
+import java.util.ArrayList;
+
+/**
+ *
+ * @author Bugaso
+ */
+public class DetalleCompraData {
+    private DetalleCompra detalleCompra;
+
+    public DetalleCompraData(DetalleCompra detalleCompra) {
+        this.detalleCompra = detalleCompra;
+    }
+    
+    
+    
+    public void MostrarDetalles(){
+        
+        System.out.println(detalleCompra.getCompra().toString());
+        
+    }
+}
